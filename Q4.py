578d472d04888b12782950521bb9c9c3aad138bc
Sharan S <98637412+sharan3102@users.noreply.github.com>
2023-06-14 00:23:52 +0530

    LongestCommonPrefix

--- Q4.py
+++ Q4.py
@@ -1,5 +1,5 @@
 class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
+    def longestCommonPrefix(self, strs: List[str]) -> str: 
         res = ""
         strs = sorted(strs)
         first = strs[0]
