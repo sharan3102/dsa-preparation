b4998f28c69d6a41b60f32882df22becc03add12
Sharan S <98637412+sharan3102@users.noreply.github.com>
2023-06-14 13:35:43 +0530

    MergeTwoLists

---
+++ Q6.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        //Approach: Using recursion
+
+        //base case
+        if(list1==null) return list2;
+        if(list2==null) return list1;
+
+        //check condition if value of list1 is less than value of list2,
+        //than we recursively add whole list2 in to list1
+        if(list1.val<list2.val){
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        }
+        else{
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+}
